7f2653b8711f3eb7c89c0c1e98e99d41ebb6c13f
c0641903 <c0641903@SB-N208-18.lambton.on.ca>
2016-01-19 09:53:44 -0500

    Created testEmptyStringShouldReturnEmptyString method

---
+++ CPD4414-Assignment1-Gadd-Dave/test/cpd4414/assignment1/gadd/dave/CPD4414Assignment1GaddDaveTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd4414.assignment1.gadd.dave;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author c0641903
+ */
+public class CPD4414Assignment1GaddDaveTest {
+    
+    public CPD4414Assignment1GaddDaveTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class CPD4414Assignment1GaddDave.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        CPD4414Assignment1GaddDave.main(args);
+    }
+
+    /**
+     * Test of stringPower method, of class CPD4414Assignment1GaddDave.
+     */
+    @Test
+    public void testEmptyStringShouldReturnEmptyString() {
+        System.out.println("testEmptyStringShouldReturnEmptyString");
+        String s = "";
+        int i = 1;
+        CPD4414Assignment1GaddDave instance = new CPD4414Assignment1GaddDave();
+        String expResult = "";
+        String result = instance.stringPower(s, i);
+        assertEquals(expResult, result);
+    }
+    
+}
